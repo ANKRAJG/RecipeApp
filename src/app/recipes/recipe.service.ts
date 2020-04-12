9ca7350314eddaa1c006d3d9382a0d108c211d04
Ankit Gupta <ankit.gupta@pype.io>
2020-04-13 01:37:34 +0530

    Attached Ingredients into Recipe Details, and Add Ingredients to Shopping List functionality done

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
     /* You can also Encapsulate this property 'recipeSelected' by providing a method 
        to get access to it, once u emit the event */
@@ -9,13 +12,28 @@ export class RecipeService {
 
     // Make this recipes array as 'private', so that you can't directly access it from outside
     private recipes: Recipe[] = [
-        new Recipe('Spicy Pasta', 'Its a great Pasta Recipe!', '../../assets/images/recipe-pasta.jpg'),
-        new Recipe('Burger', 'Its a great Burger Recipe!', '../../assets/images/burger.jfif')
+        new Recipe('Spicy Pasta', 'Its a great Pasta Recipe!', '../../assets/images/recipe-pasta.jpg',
+            [
+                new Ingredient('Potatoes', 2),
+                new Ingredient('Tomatoes', 4)
+            ]),
+        new Recipe('Big Kahuna Burger', 'Its a great Burger Recipe!', '../../assets/images/burger.jfif',
+            [
+                new Ingredient('Buns', 4),
+                new Ingredient('Potatoes', 2),
+                new Ingredient('Cheese', 2)
+            ])
     ];
 
     getRecipes() {
         // Add slice, so that we shouldn't change the recipes array from outside
         // It returns the exact copy of recipes
         return this.recipes.slice();
+    }
+
+    constructor(private slService: ShoppingListService) {}
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients);
     }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -17,4 +17,14 @@ export class ShoppingListService {
       this.ingredients.push(ingredient);
       this.ingredientsChanged.emit(this.ingredients.slice());
     }
+
+    addIngredients(ingredients: Ingredient[]) {
+      // The downside of using this for loop is that, we'll have to emit lot of ingredientsChanged events
+      // for(let ingredient of ingredients) {
+      //   this.ingredients.push(ingredient);
+      //   this.ingredientsChanged.emit(this.ingredients.slice());
+      // }
+      this.ingredients.push(...ingredients);
+      this.ingredientsChanged.emit(this.ingredients.slice());
+    }
 }
